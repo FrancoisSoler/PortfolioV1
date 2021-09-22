cc273db00456e124d8ef7a810e572ec8c64ab12b
FrancoisSoler <52791728+FrancoisSoler@users.noreply.github.com>
2021-09-23 00:36:13 +0200

    Revert "test gh page"
    This reverts commit 160755750c04176465b651622eae4d6ec8804463.

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import './scss/main.scss';
 //Animate On Scroll
-/* import AOS from 'aos';
+import AOS from 'aos';
 import './aos/dist/aos.css';
- */
+
 import * as THREE from 'three';
 //import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
@@ -186,10 +186,9 @@ animate();
 
 // AOS
 
-/* AOS.init({
+AOS.init({
 	easing: 'ease-in-sine',
 	delay: 100,
 	once:true,
 	anchorPlacement: 'top-center'
   });
- */
